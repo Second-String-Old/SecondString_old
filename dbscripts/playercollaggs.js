457bdf75b77842f37ab77fef99086abe743124d1
stcentor <skyler.tangneycentor@gmail.com>
2016-10-28 16:24:03 -0400

    some queries for sorting player data

---
+++ dbscripts/playercollaggs.js
@@ -0,0 +1,19 @@
+//filter by position
+
+db.getCollection('Players').aggregate([{
+    $match:{POS: 'WR'}  
+}])
+
+//filter by team
+
+db.getCollection('Players').aggregate([{
+    $match:{team: 'Seahawks'}  
+}])
+
+//sort by variable ascending
+//docs w/o variable are returned first 
+
+db.getCollection('Players').aggregate([{
+    $sort:{rushyards: 1}  
+}])
+
