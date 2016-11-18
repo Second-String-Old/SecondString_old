a0b21a2982965445cc587ae0843949b757706e9d
Colin Williamson <cwilliamson226@gmail.com>
2016-11-18 15:37:23 -0500

    Last minute demo fixes

--- public/javascripts/update.js
+++ public/javascripts/update.js
@@ -1,33 +1,33 @@
 var pheader = '<tr class="header">          <td><a href="javascript:sort(&quot;name&quot;, arr)">Name</a></td>          <td><a href="javascript:sort(&quot;data[i]&quot;, arr)">Team</a></td>          <td><a href="javascript:sort(&quot;POS&quot;, arr)">Position</a></td>          <td><a href="javascript:sort(&quot;gp&quot;, arr)">Games Played</a></td>          <td><a href="javascript:sort(&quot;recs&quot;, arr)">Receptions</a></td>          <td><a href="javascript:sort(&quot;yards&quot;, arr)">Yards</a></td>          <td><a href="javascript:sort(&quot;rectuddies&quot;, arr)">Reception TDs</a></td>          <td><a href="javascript:sort(&quot;rushtuddies&quot;, arr)">Rush TDs</a></td>        </tr>'
 var theader = '<tr class="header"> <td><a href="javascript:Tsort(&quot;name&quot;, arr)">Name</a></td> <td><a href="javascript:Tsort(&quot;div&quot;, arr)">Division</a></td></tr>'
-var currpos;
+var currpos = 'all';
 
 function updateTable(pos, data){
   var table = document.getElementById("table");
   table.innerHTML = pheader;
   currpos = pos;
   if(pos == 'all'){
-    for(i = 0, j=0; i < data.length, j < 10; i++){
+    for(i = 0; i < data.length; i++){
       if(data[i].name != 'name'){
-        j++;
+        // j++;
         var tr = document.createElement("tr");
         tr.innerHTML = "<tr>          <td>" + data[i].name + "</td>          <td>" + data[i].team + "</td>          <td>" + data[i].POS + "</td>          <td>" + data[i].gp + "</td>          <td>" + data[i].recs + "</td>          <td>" + (data[i].rushyards + data[i].recyards)  + "</td>          <td>" + data[i].rectuddies + "</td>          <td>" + data[i].rushtuddies + "</td>        </tr>";
         table.appendChild(tr);
       }
     }
   }else if(pos == 'FLEX'){
-    for(i = 0, j=0; i < data.length, j < 10; i++){
+    for(i = 0; i < data.length; i++){
       if(data[i].POS == 'WR' || data[i].POS == 'RB'){
-        j++;
+        // j++;
         var tr = document.createElement("tr");
         tr.innerHTML = "<tr>          <td>" + data[i].name + "</td>          <td>" + data[i].team + "</td>          <td>" + data[i].POS + "</td>          <td>" + data[i].gp + "</td>          <td>" + data[i].recs + "</td>          <td>" + (data[i].rushyards + data[i].recyards)  + "</td>          <td>" + data[i].rectuddies + "</td>          <td>" + data[i].rushtuddies + "</td>        </tr>";
         table.appendChild(tr);
       }
     }
   }else{
-    for(i = 0,j=0; i < data.length, j < 10; i++){
+    for(i = 0; i < data.length; i++){
       if(data[i].POS == pos){
-        j++;
+        // j++;
         var tr = document.createElement("tr");
         tr.innerHTML = "<tr>          <td>" + data[i].name + "</td>          <td>" + data[i].team + "</td>          <td>" + data[i].POS + "</td>          <td>" + data[i].gp + "</td>          <td>" + data[i].recs + "</td>          <td>" + (data[i].rushyards + data[i].recyards)  + "</td>          <td>" + data[i].rectuddies + "</td>          <td>" + data[i].rushtuddies + "</td>        </tr>";
         table.appendChild(tr);
