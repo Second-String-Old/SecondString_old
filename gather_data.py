ea7dc40311ffe437efa580ad9a2ec189b86cac13
Dylan Shute <shuted@rpi.edu>
2016-12-05 19:55:17 +0000

    gathered team rosters from team and player data

--- gather_data.py
+++ gather_data.py
@@ -1,4 +1,4 @@
-import httplib, urllib, base64, errno
+import httplib, urllib, base64, errno, json
 
 headers = {
     #request headers
@@ -9,16 +9,17 @@ params = urllib.urlencode({
   #none  
 })
 
+#team_data = []
 #getting team names
 try: 
     conn = httplib.HTTPSConnection('api.fantasydata.net')
     conn.request("GET", "/nfl/v2/JSON/Teams?%s" % params, "{body}", headers)
     response = conn.getresponse()
-    team_data = response.read()
-    print(team_data)
+    team_data = response.read()#json.loads(response)
+    parsed_team = json.loads(team_data)  
     conn.close()
 except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+   print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
 #getting player data
 try: 
@@ -26,9 +27,30 @@ try:
     conn.request("GET", "/nfl/v2/JSON/Players?%s" % params, "{body}", headers)
     response = conn.getresponse()
     player_data = response.read()
-    print(player_data)
+    parsed_player = json.loads(player_data)
     conn.close()
 except Exception as e:
     print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
 #cross referencing team and player data to make team rosters
+class Team:
+    def __init__(self, name, key):
+        self.roster = []
+        self.name = name
+        self.key = key
+        
+    def add_to_roster(self, playername):
+        self.roster.append(playername)
+
+teams = list()
+for team in parsed_team:
+    teams.append(Team(team['FullName'],team['Key']))
+
+for player in parsed_player:
+    for team in teams:
+        if player['Team'] == team.key:
+            team.add_to_roster(player['Name'])
+        
+for team in teams:
+    print team.name
+    print team.roster
