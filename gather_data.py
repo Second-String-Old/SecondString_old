74b95996f9e04a5b3fb7cc6e1346635dbfd5ec9c
Dylan Shute <shuted@rpi.edu>
2016-12-05 18:54:07 +0000

    Added player data

--- gather_data.py
+++ gather_data.py
@@ -15,11 +15,20 @@ try:
     conn.request("GET", "/nfl/v2/JSON/Teams?%s" % params, "{body}", headers)
     response = conn.getresponse()
     team_data = response.read()
-    print(data)
+    print(team_data)
     conn.close()
 except Exception as e:
     print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
+#getting player data
+try: 
+    conn = httplib.HTTPSConnection('api.fantasydata.net')
+    conn.request("GET", "/nfl/v2/JSON/Players?%s" % params, "{body}", headers)
+    response = conn.getresponse()
+    player_data = response.read()
+    print(player_data)
+    conn.close()
+except Exception as e:
+    print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
-
-
+#cross referencing team and player data to make team rosters
