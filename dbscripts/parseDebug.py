bf96dcbc6b29a407d9c95b4dd337ee28509751cd
cwilliamson226 <cwilliamson226@gmail.com>
2017-10-13 14:36:53 -0400

    Created new parse debugger
    Uses python to go through and compare each play to various cases, printing to a file for ones that don't match a pattern. Using this I will add more and more patterns to catch edge cases, then convert it to adding correct stats for players instead of writing to a file once finished.

---
+++ dbscripts/parseDebug.py
@@ -0,0 +1,119 @@
+import csv
+import bson
+import re
+
+def checkAgree(desc, playtype, x):
+	parsed = []
+	numbers = []
+	parsed = desc[x].split( )
+	for i in range(0, len(parsed)):
+		if parsed[i].find("-") != -1:
+			numbers.append(parsed[i][0:parsed[i].find("-")])
+	for i in range(0, len(numbers)):
+		numbers[i] = numbers[i].replace("(", "")
+		numbers[i] = numbers[i].replace("[", "")
+
+	if playtype == "PASS":
+		if len(numbers) != 3:
+			f2.write("PASS: " + desc[x])
+		else:
+			print(str(numbers))
+	elif playtype == "RUSH":
+		if len(numbers) != 2:
+			f2.write("RUSH: " + desc[x])
+		else:
+			print(str(numbers))
+	elif playtype == "SACK":
+		if len(numbers) != 2:
+			f2.write("SACK: " + desc[x])
+		else:
+			print(str(numbers))
+	elif playtype == "EXTRA POINT":
+		if len(numbers) != 3:
+			f2.write("EXTRA POINT: " + desc[x])
+		else:
+			print(str(numbers))
+	elif playtype == "KICK OFF":
+		if len(numbers) != 1 or len(numbers) != 3:
+			f2.write("KICK OFF: " + desc[x])
+		else:
+			print(str(numbers))
+	elif playtype == "PUNT":
+		if len(numbers) != 4 or len(numbers) != 2:
+			f2.write("PUNT: " + desc[x])
+		else:
+			print(str(numbers))
+	elif playtype == "SCRAMBLE":
+		if len(numbers) != 2:
+			f2.write("SCRAMBLE: " + desc[x])
+		else:
+			print(str(numbers))
+	elif playtype == "CLOCK STOP":
+		if len(numbers) != 1:
+			f2.write("CLOCK STOP: " + desc[x])
+		else:
+			print(str(numbers))
+	elif playtype == "FIELD GOAL":
+		if len(numbers) != 3:
+			f2.write("FIELD GOAL: " + desc[x])
+		else:
+			print(str(numbers))
+	elif playtype == "QB KNEEL":
+		if len(numbers) != 1:
+			f2.write("QB KNEEL: " + desc[x])
+		else:
+			print(str(numbers))
+	elif playtype == "FUMBLE":
+		if len(numbers) != 4 or len(numbers) != 2:
+			f2.write("FUMBLE: " + desc[x])
+		else:
+			print(str(numbers))
+	elif playtype == "2POINT CONVERSION":
+		if len(numbers) != 2:
+			f2.write("2POINT CONVERSION: " + desc[x])
+		else:
+			print(str(numbers))
+	else:
+		f2.write("CASE NOT FOUND - " + playtype + ": " + desc[x])
+
+def checkInt(str):
+	try:
+		str = int(str)
+	except ValueError:
+		# print("ADNVFASKJGFLKASFDJASLKJFKAJSFDKJASDKFJALKSJDFLKJASDKFJLKSAJDFLKAJSFDLKJASDKFJSAJFDKASJDFKLJASFDJSFJ\n")
+		pass
+	return str
+
+f = open('data/pbp-2016.csv')
+f2 = open('baddesc.txt', 'w')
+# f3 = open('pdesc.txt', 'w')
+csv_f = csv.reader(f)
+
+# Creates empty lists for each variable and a size counter
+offTeam = []
+defTeam = []
+desc = []
+yards = []
+pType = []
+size = 0;
+
+# Increases size, appends variables in from specific columns of the input csv
+for row in csv_f:
+	size += 1
+	offTeam.append(row[5])
+	defTeam.append(row[6])
+	desc.append(row[14])
+	yards.append(row[19])
+	pType.append(row[21])
+
+# Creates empty list for players involved (stored as QB, Receiver), Offensive team, and yards for the play
+players = []
+oTeam = []
+tYards = []
+count = 0
+
+# Goes through each play from the original lists
+for x in range(0, size):
+	checkAgree(desc, pType[x], x)
+
+# (4:58) (SHOTGUN) 13-T.SIEMIAN PASS SHORT RIGHT TO 22-C.ANDERSON TO CAR 30 FOR 21 YARDS (58-T.DAVIS).
