d005327e083571ac82b52810f1eb180eba64e79a
cwilliamson226 <cwilliamson226@gmail.com>
2017-10-25 00:11:26 -0400

    Parsing script to get the required indexes
    Using the python package we found in addition to its API, this script goes through the players in the 2016 season and creates a list of all of the indexes players have, and then adds them to the DB as the indexes.

---
+++ dbscripts/indexParser.py
@@ -0,0 +1,30 @@
+# indexParser.py
+# cwilliamson226
+# Simple Python script that uses pymongo and nflgame to create indexes in a database for all of the possible stats except for defense
+import nflgame
+from pymongo import MongoClient
+
+# Uses nflgame API to grab stats from the 2016 season
+games = nflgame.games(2016)
+plays = nflgame.combine_plays(games)
+
+# Creates a connection through pyMongo to the specified MongoDB, in this case mLab
+connection = MongoClient("mongodb://pledgemaster:skilodge@ds021356.mlab.com:21356/nfldb")
+db = connection.nfldb
+db.authenticate('pledgemaster', 'skilodge')
+
+# Goes through each player and creates a list of indexes for non-defensive players
+indexes = []
+for p in plays.players():
+	for index in p.stats:
+		i = str(index)
+		if indexes.count(i) == 0:
+			if 'defense' not in i and 'kicking' not in i and 'punting' not in i:
+				print(i)
+				indexes.append(i)
+
+# Drops the current indexes to start fresh, then goes throught the list adding all of the unique indexes to the DB
+db.nflgame_players.drop_indexes()
+for i in indexes:
+	print("2" + i)
+	db.nflgame_players.create_index(i)
