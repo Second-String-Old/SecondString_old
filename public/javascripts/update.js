11b429649bd130c3f617b9997d53963bd4ac1c3b
cwilliamson226 <cwilliamson226@gmail.com>
2017-10-27 14:50:02 -0400

    Fixed stat page and update functions to allow fully dynamic table sorting

--- public/javascripts/update.js
+++ public/javascripts/update.js
@@ -1,17 +1,18 @@
-var pheader = '<tr class="header">          <td><a href="javascript:sort(&quot;name&quot;, arr)">Name</a></td>          <td><a href="javascript:sort(&quot;data[i]&quot;, arr)">Team</a></td>          <td><a href="javascript:sort(&quot;POS&quot;, arr)">Position</a></td>          <td><a href="javascript:sort(&quot;gp&quot;, arr)">Games Played</a></td>          <td><a href="javascript:sort(&quot;recs&quot;, arr)">Receptions</a></td>          <td><a href="javascript:sort(&quot;yards&quot;, arr)">Yards</a></td>          <td><a href="javascript:sort(&quot;rectuddies&quot;, arr)">Reception TDs</a></td>          <td><a href="javascript:sort(&quot;rushtuddies&quot;, arr)">Rush TDs</a></td>        </tr>'
+// var pheader = '<tr class="header">          <td><a href="javascript:sort(&quot;name&quot;, arr)">Name</a></td>          <td><a href="javascript:sort(&quot;data[i]&quot;, arr)">Team</a></td>          <td><a href="javascript:sort(&quot;POS&quot;, arr)">Position</a></td>          <td><a href="javascript:sort(&quot;gp&quot;, arr)">Games Played</a></td>          <td><a href="javascript:sort(&quot;recs&quot;, arr)">Receptions</a></td>          <td><a href="javascript:sort(&quot;yards&quot;, arr)">Yards</a></td>          <td><a href="javascript:sort(&quot;rectuddies&quot;, arr)">Reception TDs</a></td>          <td><a href="javascript:sort(&quot;rushtuddies&quot;, arr)">Rush TDs</a></td>        </tr>'
 var theader = '<tr class="header"> <td><a href="javascript:Tsort(&quot;name&quot;, arr)">Name</a></td> <td><a href="javascript:Tsort(&quot;div&quot;, arr)">Division</a></td></tr>'
 var currpos = 'all';
+var header = '<tr class="header"> <td><a href="javascript:sort(&quot;lastname&quot;, arr)">Name</a></td> <td><a href="javascript:sort(&quot;team&quot;, arr)">Team</a></td> <td><a href="javascript:sort(&quot;pos&quot;, arr)">Position</a></td> <td><a href="javascript:sort(&quot;yards&quot;, arr)">Yards</a></td> <td><a href="javascript:sort(&quot;passing_tds&quot;, arr)">Passing TDs</a></td> <td><a href="javascript:sort(&quot;passing_int&quot;, arr)">INTs</a></td> <td><a href="javascript:sort(&quot;rectuddies&quot;, arr)">Reception TDs</a></td> <td><a href="javascript:sort(&quot;rushtuddies&quot;, arr)">Rush TDs</a></td></tr>'
 
 function updateTable(pos, data){
   var table = document.getElementById("table");
-  table.innerHTML = pheader;
+  // table.innerHTML = ''
+  table.innerHTML = header;
   currpos = pos;
   if(pos == 'all'){
     for(i = 0; i < data.length; i++){
       // j++;
       var tr = document.createElement("tr");
-      var tstring = '';
-      tstring = '<td>' + data[i].firstname + " " + data[i].lastname '</td> <td>' data[i].team '</td> <td>' data[i].pos '</td>';
+      var tstring = '<td>' + data[i].firstname + " " + data[i].lastname+ '</td> <td>'+ data[i].team +'</td> <td>'+ data[i].pos +'</td>';
       if(data[i].pos == "QB"){
         tstring = tstring + '<td>' + data[i].passing_yds + '</td>';
       }else if(data[i].pos == "WR"){
@@ -26,26 +27,49 @@ function updateTable(pos, data){
       tr.innerHTML = tstring;
       table.appendChild(tr);
     }
+  }else if(pos == 'FLEX'){
+    for(var i = 0; i < data.length; i++){
+      if(data[i].pos == 'WR' || data[i].pos == 'RB'){
+        // j++;
+        var tr = document.createElement("tr");
+        var tstring = '<td>' + data[i].firstname + " " + data[i].lastname + '</td> <td>' + data[i].team +'</td> <td>'+ data[i].pos +'</td>';
+        if(data[i].pos == "QB"){
+          tstring = tstring + '<td>' + data[i].passing_yds + '</td>';
+        }else if(data[i].pos == "WR"){
+          tstring = tstring + '<td>' + data[i].receiving_yds + '</td>';
+        }else if(data[i].pos == "RB"){
+          tstring = tstring + '<td>' + data[i].rushing_yds + '</td>';
+        }else{
+          tstring = tstring + '<td>0</td>';
+        }
+        tstring = tstring + '<td>' + data[i].passing_tds + '</td> <td>' + data[i].passing_int + '</td> <td>' + data[i].receiving_tds + '</td> <td>' + data[i].rushing_tds + '</td>';
+        // tr.innerHTML = "<tr>          <td>" + data[i].name + "</td>          <td>" + data[i].team + "</td>          <td>" + data[i].POS + "</td>          <td>" + data[i].gp + "</td>          <td>" + data[i].recs + "</td>          <td>" + data[i].pyards  + "</td>          <td>" + data[i].rectuddies + "</td>          <td>" + data[i].rushtuddies + "</td>        </tr>";
+        tr.innerHTML = tstring;
+        table.appendChild(tr);
+      }
+    }
+  }else{
+    for(i = 0; i < data.length; i++){
+      if(data[i].pos == pos){
+        // j++;
+        var tr = document.createElement("tr");
+        var tstring = '<td>' + data[i].firstname + " " + data[i].lastname +'</td> <td>'+ data[i].team+ '</td> <td>' +data[i].pos+ '</td>';
+        if(data[i].pos == "QB"){
+          tstring = tstring + '<td>' + data[i].passing_yds + '</td>';
+        }else if(data[i].pos == "WR"){
+          tstring = tstring + '<td>' + data[i].receiving_yds + '</td>';
+        }else if(data[i].pos == "RB"){
+          tstring = tstring + '<td>' + data[i].rushing_yds + '</td>';
+        }else{
+          tstring = tstring + '<td>0</td>';
+        }
+        tstring = tstring + '<td>' + data[i].passing_tds + '</td> <td>' + data[i].passing_int + '</td> <td>' + data[i].receiving_tds + '</td> <td>' + data[i].rushing_tds + '</td>';
+        // tr.innerHTML = "<tr>          <td>" + data[i].name + "</td>          <td>" + data[i].team + "</td>          <td>" + data[i].POS + "</td>          <td>" + data[i].gp + "</td>          <td>" + data[i].recs + "</td>          <td>" + data[i].pyards  + "</td>          <td>" + data[i].rectuddies + "</td>          <td>" + data[i].rushtuddies + "</td>        </tr>";
+        tr.innerHTML = tstring;
+        table.appendChild(tr);
+      }
+    }
   }
-  // else if(pos == 'FLEX'){
-  //   for(var i = 0; i < data.length; i++){
-  //     if(data[i].POS == 'WR' || data[i].POS == 'RB'){
-  //       // j++;
-  //       var tr = document.createElement("tr");
-  //       tr.innerHTML = "<tr>          <td>" + data[i].name + "</td>          <td>" + data[i].team + "</td>          <td>" + data[i].POS + "</td>          <td>" + data[i].gp + "</td>          <td>" + data[i].recs + "</td>          <td>" + data[i].pyards  + "</td>          <td>" + data[i].rectuddies + "</td>          <td>" + data[i].rushtuddies + "</td>        </tr>";
-  //       table.appendChild(tr);
-  //     }
-  //   }
-  // }else{
-  //   for(i = 0; i < data.length; i++){
-  //     if(data[i].POS == pos){
-  //       // j++;
-  //       var tr = document.createElement("tr");
-  //       tr.innerHTML = "<tr>          <td>" + data[i].name + "</td>          <td>" + data[i].team + "</td>          <td>" + data[i].POS + "</td>          <td>" + data[i].gp + "</td>          <td>" + data[i].recs + "</td>          <td>" + data[i].pyards  + "</td>          <td>" + data[i].rectuddies + "</td>          <td>" + data[i].rushtuddies + "</td>        </tr>";
-  //       table.appendChild(tr);
-  //     }
-  //   }
-  // }
 } 
 
 function updateTTable(data){
@@ -89,12 +113,12 @@ function searchTeam(data){
 
 function sort(param, data){
   //sort by name
-  if(param == 'name'){
+  if(param == 'lastname'){
     data.sort(function (a, b) {
-      if (a.name > b.name) {
+      if (a.lastname > b.lastname) {
         return 1;
       }
-      if (a.name < b.name) {
+      if (a.lastname < b.lastname) {
         return -1;
       }
       // a must be equal to b
@@ -115,12 +139,12 @@ function sort(param, data){
     });
   }
   //sort by POS
-  if(param == 'POS'){
+  if(param == 'pos'){
     data.sort(function (a, b) {
-      if (a.POS > b.POS) {
+      if (a.pos > b.pos) {
         return 1;
       }
-      if (a.POS < b.POS) {
+      if (a.pos < b.pos) {
         return -1;
       }
       // a must be equal to b
@@ -128,12 +152,12 @@ function sort(param, data){
     });
   }
   // sort by games played
-  if(param == 'gp'){
+  if(param == 'yards'){
     data.sort(function (a, b) {
-      if (a.gp > b.gp) {
+      if ((a.passing_yds + a.rushing_yds + a.receiving_yds) > (b.passing_yds + b.rushing_yds + b.receiving_yds)) {
         return 1;
       }
-      if (a.gp < b.gp) {
+      if ((a.passing_yds + a.rushing_yds + a.receiving_yds) < (b.passing_yds + b.rushing_yds + b.receiving_yds)) {
         return -1;
       }
       // a must be equal to b
@@ -141,25 +165,12 @@ function sort(param, data){
     });
   }
   // sort by receptions
-  if(param == 'recs'){
+  if(param == 'passing_tds'){
     data.sort(function (a, b) {
-      if (a.recs > b.recs) {
+      if (a.passing_tds > b.passing_tds) {
         return 1;
       }
-      if (a.recs < b.recs) {
-        return -1;
-      }
-      // a must be equal to b
-      return 0;
-    });
-  }
-  // sort by yards
-  if(param == 'yards'){
-    data.sort(function (a, b) {
-      if ((a.rushyards + a.recyards) > (b.rushyards + b.recyards)) {
-        return 1;
-      }
-      if ((a.rushyards + a.recyards) < (b.rushyards + b.recyards)) {
+      if (a.passing_tds < b.passing_tds) {
         return -1;
       }
       // a must be equal to b
@@ -167,12 +178,12 @@ function sort(param, data){
     });
   }
   // sort by rectuddies
-  if(param == 'rectuddies'){
+  if(param == 'passing_int'){
     data.sort(function (a, b) {
-      if (a.rectuddies > b.rectuddies) {
+      if (a.passing_int > b.passing_int) {
         return 1;
       }
-      if (a.rectuddies < b.rectuddies) {
+      if (a.passing_int < b.passing_int) {
         return -1;
       }
       // a must be equal to b
