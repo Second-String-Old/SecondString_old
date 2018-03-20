ce2730e4f4bf27494cb48d549e177447114ac398
cwilliamson226 <cwilliamson226@gmail.com>
2018-03-20 16:38:22 -0400

    Adding dynamic port listening

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require("express");
 const http = require("http");
 const app = express();
 const server = http.createServer(app);
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log("HTTP server listening on port 3000");
 });
 
